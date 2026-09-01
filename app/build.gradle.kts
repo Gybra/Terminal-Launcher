036910ca4abd90ec8cba7e145249e2de75153957
Oreste Acacia <oresteacacia@gmail.com>
2026-09-02 00:51:30 +0200

    chore: bump the version to 0.1.0-alpha04

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.gybra.terminallauncher"
         minSdk = 28
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.1.0-alpha03"
+        versionCode = 4
+        versionName = "0.1.0-alpha04"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
