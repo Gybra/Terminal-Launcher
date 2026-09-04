e000489df682d39e93995ca2c9a30cfe6e148f3d
Oreste Acacia <oresteacacia@gmail.com>
2026-09-04 22:54:51 +0200

    chore: bump the version to 0.1.0-alpha06
    Android will not install this APK over alpha05 unless the version code
    is higher.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.gybra.terminallauncher"
         minSdk = 28
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.1.0-alpha05"
+        versionCode = 6
+        versionName = "0.1.0-alpha06"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
